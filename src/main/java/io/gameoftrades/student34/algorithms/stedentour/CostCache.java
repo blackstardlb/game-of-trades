5c7b2c320ebcff2b8b91a33c627283d5a423d163
blackstardlb <brisondl@gmail.com>
2016-10-25 16:07:03 +0200

    Changed doublemapkey to subclass of costcache

--- src/main/java/io/gameoftrades/student34/algorithms/stedentour/CostCache.java
+++ src/main/java/io/gameoftrades/student34/algorithms/stedentour/CostCache.java
@@ -38,7 +38,7 @@ public class CostCache {
         costs.clear();
     }
 
-    public static class StadWrapper {
+    private static class StadWrapper {
 
         private final Stad stad;
 
@@ -61,4 +61,33 @@ public class CostCache {
             return obj != null && obj instanceof StadWrapper && obj.toString().equalsIgnoreCase(toString());
         }
     }
+
+    private static class DoubleMapKey<T> {
+
+        private final T x;
+        private final T y;
+
+        DoubleMapKey(T x, T y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o != null && o instanceof DoubleMapKey) {
+                DoubleMapKey key = (DoubleMapKey) o;
+                return (this.x.equals(key.x) && this.y.equals(key.y));
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 17;
+            result = result * 37 + x.hashCode();
+            return result * 37 + y.hashCode();
+        }
+    }
+
 }
