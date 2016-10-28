200e6dc5fc5a826043f8d4cffc7871aff285829d
EcMiner <ecminer90@gmail.com>
2016-10-28 16:48:05 +0200

    DoubleMapKey merge issues gefixed

--- src/main/java/io/gameoftrades/student34/algorithms/stedentour/CostCache.java
+++ src/main/java/io/gameoftrades/student34/algorithms/stedentour/CostCache.java
@@ -132,4 +132,38 @@ public class CostCache {
             return obj != null && obj instanceof StadWrapper && obj.toString().equalsIgnoreCase(toString());
         }
     }
+
+    /**
+     * Deze class kan worden gebruikt als key voor bijvoorbeeld een {@link java.util.Map} of een {@link java.util.List}.
+     *
+     * @param <T>
+     * @see CostCache
+     */
+    private static class DoubleMapKey<T> {
+
+        private final T x;
+        private final T y;
+
+        DoubleMapKey(T x, T y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o != null && o instanceof DoubleMapKey) {
+                DoubleMapKey key = (DoubleMapKey) o;
+                return (this.x.equals(key.x) && this.y.equals(key.y));
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 17;
+            result = result * 37 + x.hashCode();
+            return result * 37 + y.hashCode();
+        }
+    }
 }
